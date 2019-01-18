98eecf9c2b4cb22f21c1b19ea66e7f1621f48406
Evangelos <eliatsas@haf.gr>
2019-01-18 11:53:52 +0200

    added archive producer and consumer, mongodb archive collection

--- smq/src/archive/consumer.js
+++ smq/src/archive/consumer.js
@@ -2,29 +2,22 @@
 
 const config = require('../config/config')
 const redisSMQ = require('redis-smq')
-const MongoClient = require('mongodb').MongoClient
-const io = require('socket.io')(config.socket.port)
+const { MongoClient } = require('mongodb')
 const redisAdapter = require('socket.io-redis')
-
-io.adapter(
-  redisAdapter({
-    host: config.smq.redis.host,
-    port: config.smq.redis.port
-  })
-)
 
 const { Consumer } = redisSMQ
 
 class ArchiveConsumer extends Consumer {
   constructor() {
+    const dbName = config.smq.mongodb.database
     const url = `mongodb://${config.smq.mongodb.host}:${
       config.smq.mongodb.port
-    }`
-    const dbName = config.smq.mongodb.database
+    }/${dbName}`
+
     const client = new MongoClient(url)
-    client.connect(function(err, client) {
+    client.connect((err, client) => {
       const db = client.db(dbName)
-      const archive = db.collection('archive')
+      this.archive = db.collection('archive')
     })
 
     super()
@@ -36,30 +29,15 @@ class ArchiveConsumer extends Consumer {
       message.d.event.competition
     }:${message.d.event.sport}:${new Date().getFullYear()}`
 
-    let count = await archive.count({ key: msgKey })
-    const exists = count > 0
-    if (exists) {
-      //fetch the existing data for the event
-      var data = await archive.findOne({ key: msgKey })
-      //update the values of the event from the values of the message
-      const lastEvent = data.d.events[data.d.events.length - 1]
-      if (lastEvent.time !== message.d.event.time) {
-        lastEvent.push(message.d.event)
-      }
-      const lastLive = data.d.lives[data.d.lives.length - 1]
-      if (lastLive.time !== message.d.live.time) {
-        lastLive.push(message.d.live)
-      }
-      for (var index in data.d.markets) {
-        //data.d.markets[index] = message.d.markets[index]
-      }
-      data.d.meta.gameSummary = message.d.meta.gameSummary
-      //if the event status has changed
-
-      //replace the existing event data
-    } else {
-      //no existing data for the given event
-      //insert the event data from the message
+    const entry = {
+      key: msgKey,
+      timestamp: new Date(),
+      message: JSON.stringify(message)
+    }
+    try {
+      await this.archive.insertOne(entry)
+    } catch (err) {
+      console.log(err)
     }
 
     cb()
